f4d6a8ea4ea12a274e2672772afb5ba950a23900
David Lazarte <david.lazarte@bymovi.com>
2017-12-28 16:41:38 -0300

    Several fixes

--- src/ios/IOSCheckPermissions.h
+++ src/ios/IOSCheckPermissions.h
@@ -1,55 +1,29 @@
-//
-//  Created by Douglas Frari on 7/13/15.
-//
-//  The MIT License (MIT)
-//
-//  Copyright (c) 2015 Douglas Frari
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in all
-//  copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//  SOFTWARE.
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
 
 #import <UIKit/UIKit.h>
-#import <Foundation/Foundation.h>
-#import <EventKit/EventKit.h>
-#import <AssetsLibrary/AssetsLibrary.h>
 #import <AVFoundation/AVFoundation.h>
-#import <CoreLocation/CoreLocation.h>
-#import <Cordova/CDV.h>
-
-
-/**
- * Type for request the location authorization (AlwaysAuthorization or WhenInUseAuthorization)
- */
-enum AuthorizeRequestType {
-    AuthorizeRequestTypeAlwaysAuthorization = 1,
-    AuthorizeRequestTypeWhenInUseAuthorization = 2    
-};
-
+#import <AssetsLibrary/AssetsLibrary.h>
+#import <Cordova/CDVPlugin.h>
 
 @interface IOSCheckPermissions : CDVPlugin
-
-/**
- *  Get the global instance of ios-check-permission library.
- *
- *  @return instance of IOSCheckPermissions
- */
-+ (instancetype) globalInstance;
-
+{}
 
 /**
  *  Enable logs for using the library with monitoring of your functionality. 
@@ -59,9 +33,9 @@ enum AuthorizeRequestType {
  */
 - (void)enableCheckPermissionLogs:(BOOL)enableLogs;
 
+- (void)checkPermissionAccessForPhotos:(CDVInvokedUrlCommand*)command;
 - (void)checkPermissionAccessForCalendar:(CDVInvokedUrlCommand*)command;
 - (void)checkPermissionAccessForReminder:(CDVInvokedUrlCommand*)command;
-- (void)checkPermissionAccessForGallery:(CDVInvokedUrlCommand*)command;
 - (void)checkPermissionAccessForLocation:(CDVInvokedUrlCommand*)command;
 
 @end
--- src/ios/IOSCheckPermissions.m
+++ src/ios/IOSCheckPermissions.m
@@ -1,40 +1,27 @@
-//
-//  Created by Douglas Frari on 7/13/15.
-//
-//  The MIT License (MIT)
-//
-//  Copyright (c) 2015 Douglas Frari
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in all
-//  copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//  SOFTWARE.
+/*
+ Licensed to the Apache Software Foundation (ASF) under one
+ or more contributor license agreements.  See the NOTICE file
+ distributed with this work for additional information
+ regarding copyright ownership.  The ASF licenses this file
+ to you under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License.  You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ KIND, either express or implied.  See the License for the
+ specific language governing permissions and limitations
+ under the License.
+ */
 
-#import "IOSCheckPermissions.h"
 #import <Cordova/CDV.h>
+#import "IOSCheckPermissions.h"
 
-@interface IOSCheckPermissions() <CLLocationManagerDelegate>
-
-@property (strong,nonatomic) CLLocationManager *locationManagerFromLib;
-@property (assign,nonatomic) BOOL checkingLocationPermission;
-@property (assign,nonatomic) enum AuthorizeRequestType type;
-@property (strong,nonatomic) id localizationFailureBlock;
-@property (strong,nonatomic) id localizationSuccessBlock;
-@property (assign,nonatomic) CLAuthorizationStatus authorizationStatus;
-
+@interface IOSCheckPermissions()
+{}
 @end
 
 @implementation IOSCheckPermissions
@@ -53,7 +40,6 @@ static BOOL isEnableLogs = NO;
     return sharedInstance;
 }
 
-
 - (void)enableCheckPermissionLogs:(BOOL)enableLogs {
     isEnableLogs = enableLogs;
     
@@ -62,12 +48,10 @@ static BOOL isEnableLogs = NO;
     }
 }
 
-- (void)checkPermissionAccessForCalendar:(CDVInvokedUrlCommand*)command {
-    
-}
+- (void)checkPermissionAccessForCalendar:(CDVInvokedUrlCommand*)command {}
 
-- (void)checkPermissionAccessForGallery:(CDVInvokedUrlCommand*)command {
-    CDVPluginResult* pluginResult = nil;
+- (void)checkPermissionAccessForPhotos:(CDVInvokedUrlCommand *) command {
+    BOOL status = NO;
     
     @try {
         ALAuthorizationStatus authStatus = [ALAssetsLibrary authorizationStatus];
@@ -77,26 +61,27 @@ static BOOL isEnableLogs = NO;
                 NSLog(@"- checkPermissionAccessForGallery - successBlock (status: Authorized)");
             }
             
-            pluginResult = [CDVPluginResult resultWithStatus:YES];
+            status = YES;
         }
         else if (authStatus == AVAuthorizationStatusNotDetermined) {
-            
-            ALAssetsLibrary *assetsLibrary = [[ALAssetsLibrary alloc] init];
-            
-        } else {
+           
+        }
+        else {
             if (isEnableLogs) {
-                NSLog(@"- - checkPermissionAccessForGallery - failureBlock (status: Restricted or Denied)");
+                NSLog(@"Photo Library: Restricted or Denied");
             }
-            
-            pluginResult = [CDVPluginResult resultWithStatus:NO];
         }
     } @catch (NSException *exception) {
         if (isEnableLogs) {
-            NSLog(@"- - checkPermissionAccessForGallery - failureBlock (status: Restricted or Denied)");
+            NSLog(@"Photo Library: Restricted or Denied");
         }
-        
-        pluginResult = [CDVPluginResult resultWithStatus:NO];
     }
+    
+    NSDictionary *results = @{
+                              @"hasPermission" : [NSNumber numberWithBool:status]
+                              };
+    
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:results];
     
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
